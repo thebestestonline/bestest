09f503aee796e69dc5d63e86d8efdb11d22915c4
Tyler Williams <thebestestonline@gmail.com>
2017-04-27 01:26:31 +0000

    imporved error messages

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -3,15 +3,19 @@ class ContactsController < ApplicationController
   
   def new
   @contact = Contact.new
-end
-def create
-  @contact = Contact.new(contact_params)
-  if @contact.save
-     redirect_to new_contact_path, notice: "Message sent."
-  else
-     redirect_to new_contact_path, notice: "Error occured."
   end
-end
+
+    def create
+      @contact = Contact.new(contact_params)
+      if @contact.save
+         flash[:success] = "Message sent."
+         redirect_to new_contact_path
+      else
+         flash[:error] = @contact.errors.full_messages.join(", ")
+         redirect_to new_contact_path
+      end
+    end
+
 private
   def contact_params
      params.require(:contact).permit(:name, :email, :comments)
