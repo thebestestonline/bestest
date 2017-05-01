36ee9b8bd25e52cd5c28097de597d58a0808b59e
Tyler Williams <thebestestonline@gmail.com>
2017-05-01 04:42:45 +0000

    added conditional logic on signup form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
+ #added a global variables for joing the two plans
+ 
     before_filter :beforeFilter
         def beforeFilter
             $basic_plan = Plan.find(1)
