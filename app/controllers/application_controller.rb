fc2099abcfade2147cfdac9a8d544034682c153b
Tyler Williams <thebestestonline@gmail.com>
2017-05-02 16:38:53 +0000

    Added server side code for creating subscriptions for pro users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,20 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
  #added a global variables for joing the two plans
- 
-    before_filter :beforeFilter
-        def beforeFilter
+        
+#white listing form fields        
+    before_action :configure_permitted_parameters, if: :devise_controller?
+    protected
+    
+        def configure_permitted_parameters
+            devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit( :stripe_card_token, :email, :password, :password_confirmation) }
+        end
+        
+        before_action :beforeAction
+        def beforeAction
             $basic_plan = Plan.find(1)
             $pro_plan = Plan.find(2)
-        end 
+        end
   
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
     root to: 'pages#home'
+    devise_for :users, controllers: { registrations: 'users/registrations' }
     get 'about', to: 'pages#about'
     get 'get-listed', to: 'pages#get-listed'
     resources :contacts, only: :create
     get 'contact-us', to: 'contacts#new', as: 'new_contact'
-    devise_for :users
+    
 end
