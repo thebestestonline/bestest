362944d8ef428a8ec67e9c7a4b01ca0e561e26e7
Tyler Williams <thebestestonline@gmail.com>
2017-05-04 01:49:56 +0000

    leftovers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,15 @@ class UsersController < ApplicationController
   
   before_action :authenticate_user!
   
+  def index
+    @users = User.all
+  end
+  
+  
   # GET to /users/:id
   def show
     @user = User.find( params[:id] )
   end
+  
   
 end
