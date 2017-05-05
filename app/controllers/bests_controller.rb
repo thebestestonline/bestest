f89cd4ed510548f662953cb785ae1d87a14af878
Tyler Williams <thebestestonline@gmail.com>
2017-05-05 19:38:20 +0000

    changed lots of stuff

--- app/controllers/bests_controller.rb
+++ app/controllers/bests_controller.rb
@@ -1,12 +1,12 @@
 class BestsController < ApplicationController
 
-  before_filter :set_user, only: [:show]
+  before_action :set_user, only: [:show]
     def set_user
      @u = User.find( params[:user_id] )
      @b = @u.bests.find( params[:id] )
     end
     
-  before_filter :set_index, only: [:index]
+  before_action :set_index, only: [:index]
     def set_index
      @u = User.find( params[:user_id] )
      @bests = @u.bests.all
@@ -18,31 +18,30 @@ class BestsController < ApplicationController
   end
 
 # POST to /users/:user_id/bests
-
-    def create
-      # Ensure that we have the user who is filling out form
-        @user = User.find( params[:user_id] )
-        @best = @user.bests.build(best_params)
-        if @best.save
-          flash[:success] = "Keyword Saved!"
-          redirect_to user_bests_path(current_user.id)
-        end
-    end
-    
-    private
-      def best_params
-        params.require(:best).permit( :term, :rank, :company_name, :short_description,
-        :long_description,  :email, :address, :phone_number  )
+  def create
+    # Ensure that we have the user who is filling out form
+      @user = User.find( params[:user_id] )
+      @best = @user.bests.build(best_params)
+      if @best.save
+        flash[:success] = "Keyword Saved!"
+        redirect_to user_bests_path(current_user.id)
       end
+  end
+    
+  private
+    def best_params
+      params.require(:best).permit( :term, :rank, :company_name, :short_description,
+      :long_description,  :email, :address, :phone_number  )
+    end
       
-#GET to /users/:user_id/bests/:id
-        
-    def show
-    end
-    
-    
-    def index
-    end
+
+#GET to /users/:user_id/bests/:id        
+  def show
+  end
+  
+#GET to /users/:user_id/bests/ 
+  def index
+  end
    
    
 
