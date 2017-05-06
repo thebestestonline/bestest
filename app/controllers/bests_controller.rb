cf3e65737a9095929cebbd880830df397b7e48b1
Tyler Williams <thebestestonline@gmail.com>
2017-05-06 03:06:53 +0000

    added deleting function to bests index

--- app/controllers/bests_controller.rb
+++ app/controllers/bests_controller.rb
@@ -10,7 +10,7 @@ def require_permission
 end
 
 
-  before_action :set_user, only: [:show]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
     def set_user
      @u = User.find( params[:user_id] )
      @b = @u.bests.find( params[:id] )
@@ -21,6 +21,25 @@ end
      @u = User.find( params[:user_id] )
      @bests = @u.bests.all
     end
+    
+  def update
+    if @b.update_attributes(best_params)
+      flash[:success] = "Keyword Profile Updated!"
+      redirect_to user_best_path(current_user.id,@b.id)
+    else
+        render action: :edit
+    end
+  end
+  
+  def destroy
+    if @b.delete
+      flash[:success] = "Keyword Profile Deleted!"
+      redirect_to user_bests_path(current_user.id)
+    else
+        
+    end
+  end
+
 
 # GET to /users/:user_id/best/new
   def new
@@ -52,6 +71,12 @@ end
 #GET to /users/:user_id/bests/ 
   def index
   end
+  
+    
+#GET to /users/:user_id/bests/:id/edit
+  def edit
+  end
+   
    
    
 
